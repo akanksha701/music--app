1960170d11fadfebad439af6aeb68649c172cf77
akanksha701 <akanksha@upforce.tech>
2024-12-03 15:44:24 +0530

    lazy loading added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import { ClerkProvider } from '@clerk/nextjs';
-import './globals.css';
-import { NextUIProvider } from '@nextui-org/react';
-import { Toaster } from 'react-hot-toast';
-import ReduxProvider from '@/Redux/storeProvider';
-import NavbarPage from './Navbar/index';
-import { ThemeProvider } from 'next-themes';
-import Footer from './Footer/Footer';
-
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { Toaster } from "react-hot-toast";
+import ReduxProvider from "@/Redux/storeProvider";
+import NavbarPage from "./Navbar/index";
+import { ThemeProvider } from "next-themes";
+import Footer from "./Footer/Footer";
+import { Suspense } from "react";
+import Loading from "./loading";
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const res = await fetch('http://localhost:3000/api/marketing');
+  const res = await fetch("http://localhost:3000/api/marketing");
   const data = await res.json();
 
   return (
@@ -21,11 +22,11 @@ export default async function RootLayout({
         <html lang="en">
           <body className="min-h-screen flex flex-col">
             <Toaster position="top-center" />
-            <NavbarPage />
-            <NextUIProvider>
-              {children}
-            </NextUIProvider>
-            <Footer data={data?.footerContent} />
+            <Suspense fallback={<Loading/>}>
+              <NavbarPage />
+              <NextUIProvider>{children}</NextUIProvider>
+              <Footer data={data?.footerContent} />
+            </Suspense>
           </body>
         </html>
       </ReduxProvider>
