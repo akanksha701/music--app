c34c65071d4dc749d53121ec6cbe76b06a080a55
akanksha701 <akanksha@upforce.tech>
2024-12-18 15:56:03 +0530

    pve

--- commands.ts
+++ commands.ts
@@ -1,3 +1,8 @@
 //es lint check error - npx eslint .
 // es lint error fix- npm run lint -- --fix
 
+// scroll transparent
+// image of music player 
+// card design
+//caraousal reoved 
+// duration bug
