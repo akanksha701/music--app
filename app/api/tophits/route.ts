dfd7643fe22ba43586165290cc30c008bc26de66
akanksha701 <akanksha@upforce.tech>
2025-01-02 16:30:38 +0530

    audio file peak api changes

--- app/api/tophits/route.ts
+++ app/api/tophits/route.ts
@@ -4,18 +4,18 @@ import Music from "@/lib/models/Music";
 import { currentUser } from "@clerk/nextjs/server";
 import { IMusicProps } from "@/app/(BrowsePage)/Browse/types/types";
 import { lastValueFrom, Observable } from "rxjs";
-import fs from 'fs';
-import path from 'path';
-import decode from 'audio-decode'; 
+import fs from "fs";
+import path from "path";
+import decode from "audio-decode";
 
-const audioDirectory = path.join(process.cwd(), 'public');
+const audioDirectory = path.join(process.cwd(), "public");
 
 async function decodeAudioFile(filepath: string): Promise<Float32Array[]> {
   const audioData = await fs.promises.readFile(filepath);
 
   try {
     const decodedAudio = await decode(audioData);
-    
+
     const channelData: Float32Array[] = [];
     for (let i = 0; i < decodedAudio.numberOfChannels; i++) {
       channelData.push(decodedAudio.getChannelData(i)); // Get data for each channel
@@ -24,64 +24,78 @@ async function decodeAudioFile(filepath: string): Promise<Float32Array[]> {
     return channelData;
   } catch (error) {
     console.error(`Error decoding audio file: ${filepath}`, error);
-    throw error;  // Re-throw the error after logging
+    throw error; // Re-throw the error after logging
   }
 }
 
-
-function processAudioPeaks(channelData: Float32Array[], bufferSize: number): number[] {
+function processAudioPeaks(
+  channelData: Float32Array[],
+  bufferSize: number
+): number[] {
   const peaks: number[] = [];
 
   channelData.forEach((data) => {
     const sampleCount = Math.floor(data.length / bufferSize);
-    
+
     for (let i = 0; i < sampleCount; i++) {
       const chunk = data.slice(i * bufferSize, (i + 1) * bufferSize);
-      const peak = Math.max(...chunk); 
+      const peak = Math.max(...chunk);
       peaks.push(peak);
     }
   });
-
-  return peaks;
+  const min = Math.min(...peaks);
+  const max = Math.max(...peaks);
+  return peaks.map(peak => (peak - min) / (max - min)); 
 }
-export function getAudioPeaks(filepath: string, bufferSize: number): Observable<number[]> {
+export function getAudioPeaks(
+  filepath: string,
+  bufferSize: number
+): Observable<number[]> {
   return new Observable((observer) => {
     const fileExtension = path.extname(filepath).toLowerCase();
 
-    if (fileExtension === '.wav' || fileExtension === '.mp3') {
-      decodeAudioFile(filepath).then((decodedAudio) => {
-        const peaks = processAudioPeaks(decodedAudio, bufferSize);
-        observer.next(peaks);
-        observer.complete();
-      }).catch((err) => {
-        observer.error(err);
-      });
+    if (fileExtension === ".wav" || fileExtension === ".mp3") {
+      decodeAudioFile(filepath)
+        .then((decodedAudio) => {
+          const peaks = processAudioPeaks(decodedAudio, bufferSize);
+          observer.next(peaks);
+          observer.complete();
+        })
+        .catch((err) => {
+          observer.error(err);
+        });
     } else {
       observer.error(new Error("Unsupported file format"));
     }
   });
 }
 
-async function getMusicWithPeaks(musics: IMusicProps[], audioDirectory: string) {
+async function getMusicWithPeaks(
+  musics: IMusicProps[],
+  audioDirectory: string
+) {
   const musicWithPeaks = await Promise.all(
     musics.map(async (music: IMusicProps) => {
       const audioFileUrl = music.audioUrl;
 
-      if (!audioFileUrl || (!audioFileUrl.endsWith(".mp3") && !audioFileUrl.endsWith(".wav"))) {
+      if (
+        !audioFileUrl ||
+        (!audioFileUrl.endsWith(".mp3") && !audioFileUrl.endsWith(".wav"))
+      ) {
         return { ...music, peaks: [] };
       }
 
-      const filepath = path.join(audioDirectory, audioFileUrl);
+      const filepath = await path.join(audioDirectory, audioFileUrl);
       if (!fs.existsSync(filepath)) {
         console.error(`File not found at path: ${filepath}`);
         return { ...music, peaks: [] };
       } else {
         try {
-          const peaks = await lastValueFrom(getAudioPeaks(filepath, 512));
-          return { ...music, peaks }; 
+          const peaks = await lastValueFrom(await getAudioPeaks(filepath, 512));
+          return { ...music, peaks };
         } catch (error) {
           console.error("Error generating peaks:", error);
-          return { ...music, peaks: [] }; 
+          return { ...music, peaks: [] };
         }
       }
     })
@@ -94,7 +108,7 @@ export async function GET() {
   try {
     await dbConnect();
     const user: any = await currentUser();
-    
+
     const musics = await Music.aggregate([
       {
         $lookup: {
@@ -205,7 +219,6 @@ export async function GET() {
     ]);
 
     const musicWithPeaks = await getMusicWithPeaks(musics, audioDirectory);
-    console.log("musicWithPeaks", musicWithPeaks);
 
     return NextResponse.json({ status: 200, data: musicWithPeaks });
   } catch (error) {
