708141481e367ae295824d92d08c95cd108af1c3
akanksha701 <akanksha@upforce.tech>
2025-01-07 17:31:49 +0530

    file path added

--- lib/firebase/firebaseAdmin/auth.ts
+++ lib/firebase/firebaseAdmin/auth.ts
@@ -1,10 +1,11 @@
 import admin from 'firebase-admin';
 import path from 'path';
 
+const serviceAccount = path.join(process.cwd(), 'lib/firebase/music-app-cd5ac-firebase-adminsdk-gjhw0-3b8d684838.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({}),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
