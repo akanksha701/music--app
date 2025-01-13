9dfccbd6d80f69ba6cab7f5ccc400f8de27bdcf6
akanksha701 <akanksha@upforce.tech>
2025-01-13 17:33:17 +0530

    env import changes

---
+++ error.tsx
@@ -0,0 +1,11 @@
+// app/error.tsx
+
+export default function ErrorPage() {
+  return (
+    <div>
+      <h1>An error occurred!</h1>
+      <p>Sorry for the inconvenience. Please try again later.</p>
+    </div>
+  );
+}
+  
