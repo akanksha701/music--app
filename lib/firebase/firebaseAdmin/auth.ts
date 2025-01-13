e0b50242d3ec54d1e1148d2931692df87fca45ec
akanksha701 <akanksha@upforce.tech>
2025-01-13 13:11:06 +0530

    env changes

--- lib/firebase/firebaseAdmin/auth.ts
+++ lib/firebase/firebaseAdmin/auth.ts
@@ -1,7 +1,17 @@
 import admin from 'firebase-admin';
-import path from 'path';
 
-const serviceAccount = path.join(process.cwd(), 'lib/firebase/music-app-cd5ac-firebase-adminsdk-gjhw0-3b8d684838.json');
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  projectId: process.env.PROJECT_ID,
+  privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Replace escaped newlines with actual newlines
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  clientId: process.env.FIREBASE_CLIENT_ID,
+  authUri: process.env.FIREBASE_AUTH_URI,
+  tokenUri: process.env.FIREBASE_TOKEN_URI,
+  authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
