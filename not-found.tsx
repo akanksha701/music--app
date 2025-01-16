9a3abf7f79a25350d7e1230fccc70ecffed58bc6
akanksha701 <akanksha@upforce.tech>
2025-01-16 11:58:18 +0530

    suspence wrapped

--- not-found.tsx
+++ not-found.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import {Suspense} from 'react';
  
 export default function NotFound() {
   return (
     <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <Suspense fallback={<>Loading...</>}>
+        <h2>Not Found</h2>
+        <p>Could not find requested resource</p>
+        <Link href="/">Return Home</Link>
+      </Suspense>
+    
     </div>
   );
 }
