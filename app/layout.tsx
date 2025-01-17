8c85c3c82dfdde5db74c65e4e1fabe0993c24425
akanksha701 <akanksha@upforce.tech>
2025-01-17 16:01:31 +0530

    public files deleted

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,36 +3,45 @@ import { Tooltip } from '@nextui-org/react';
 import { Toaster } from 'react-hot-toast';
 import ReduxProvider from '@/Redux/storeProvider';
 import NavbarPage from './Navbar/index';
-// import Footer from './Footer/Footer';
+import Footer from './Footer/Footer';
 import MusicPlayerContainer from './Music/UI/UtilityComponent/MusicPlayerContainer';
-// import { Nunito } from 'next/font/google';
 import { Suspense } from 'react';
-// const nunito = Nunito({
-//   subsets: ['latin'],
-//   weight: ['300', '400', '600', '700'],
-// });
+import Loading from './Album/loading';
+
+// Helper function to fetch data
+async function fetchFooterData() {
+  try {
+    const res = await fetch('http://localhost:3000/api/marketing', { cache: 'no-store' });  
+    if (!res.ok) {
+      return null;
+    }
+    return res.json();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return null;  
+  }
+}
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
-
-  // const res = await fetch('http://localhost:3000/api/marketing');
-  // const data = await res.json();
+  const footerData = await fetchFooterData();
 
   return (
     <ReduxProvider>
       <html lang="en">
-        <body className={'min-h-screen flex flex-col'}>
+        <body className="min-h-screen flex flex-col">
           <Toaster position="top-center" />
           <Tooltip />
-          <Suspense fallback={<></>}>
+          <Suspense fallback={<Loading />}>
             <NavbarPage />
             <main className="flex-grow">{children}</main>
             <MusicPlayerContainer />
-            {/* <Footer data={data?.footerContent} /> */}
+            {
+              footerData ? <Footer data={footerData?.footerContent} /> : <></>
+            }
           </Suspense>
         </body>
       </html>
