6d1bc2a1fff435050d2707d557ae3247b1cde224
akanksha701 <akanksha@upforce.tech>
2025-01-17 15:41:55 +0530

    footer commented

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,10 @@ import { Tooltip } from '@nextui-org/react';
 import { Toaster } from 'react-hot-toast';
 import ReduxProvider from '@/Redux/storeProvider';
 import NavbarPage from './Navbar/index';
-import Footer from './Footer/Footer';
+// import Footer from './Footer/Footer';
 import MusicPlayerContainer from './Music/UI/UtilityComponent/MusicPlayerContainer';
 // import { Nunito } from 'next/font/google';
 import { Suspense } from 'react';
-import Loading from './Album/loading';
 
 
 // const nunito = Nunito({
@@ -22,8 +21,8 @@ export default async function RootLayout({
 }>) {
   
 
-  const res = await fetch('http://localhost:3000/api/marketing');
-  const data = await res.json();
+  // const res = await fetch('http://localhost:3000/api/marketing');
+  // const data = await res.json();
 
   return (
     <ReduxProvider>
@@ -31,11 +30,11 @@ export default async function RootLayout({
         <body className={'min-h-screen flex flex-col'}>
           <Toaster position="top-center" />
           <Tooltip />
-          <Suspense fallback={<Loading />}>
+          <Suspense fallback={<></>}>
             <NavbarPage />
             <main className="flex-grow">{children}</main>
             <MusicPlayerContainer />
-            <Footer data={data?.footerContent} />
+            {/* <Footer data={data?.footerContent} /> */}
           </Suspense>
         </body>
       </html>
