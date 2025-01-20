e8618bc09078b7c7590291cb35a5015176eb4bdd
akanksha701 <akanksha@upforce.tech>
2025-01-20 11:30:28 +0530

    error component

--- error.tsx
+++ error.tsx
@@ -1,33 +1,9 @@
-'use client'
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-interface Props {
-  children: React.ReactNode;
-  fallback: React.ReactNode;  // The fallback UI when an error occurs
-}
-
-const ErrorBoundary: React.FC<Props> = ({ children, fallback }) => {
-  const [hasError, setHasError] = useState(false);
-
-  useEffect(() => {
-    const handleError = (error: Error) => {
-      setHasError(true);
-      console.error('Caught an error:', error);
-    };
-
-    // Catch any uncaught errors
-    window.addEventListener('error', (event) => handleError(event.error));
-
-    return () => {
-      window.removeEventListener('error', (event) => handleError(event.error));
-    };
-  }, []);
-
-  if (hasError) {
-    return <>{fallback}</>;  // Return the fallback UI when error occurs
-  }
-
-  return <>{children}</>;
+const Error = () => {
+  return (
+    <div>error</div>
+  );
 };
 
-export default ErrorBoundary;
+export default Error;
