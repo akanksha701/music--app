0639c14cc3d03100c71ff303a1c3c8730081b7f5
akanksha701 <akanksha@upforce.tech>
2025-01-21 12:06:18 +0530

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,12 +27,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const footerData = await fetchFooterData();
-  // const fallback = (
-  //   <div className="error-container">
-  //     <h1>Something went wrong</h1>
-  //     <p>Please try again later.</p>
-  //   </div>
-  // );
+  
   return (
     <ReduxProvider>
       <html lang="en">
