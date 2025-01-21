a14a95ab8f09a2597a1ed200cbff12a23806a177
akanksha701 <akanksha@upforce.tech>
2025-01-21 23:04:56 +0530

    imp chnages

--- app/(BrowsePage)/Browse/UI/UtilityComponent/MusicPlayCard.tsx
+++ app/(BrowsePage)/Browse/UI/UtilityComponent/MusicPlayCard.tsx
@@ -5,9 +5,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { redirect } from 'next/navigation';
 import { RootState } from '@/Redux/store';
 import MemoizedMusicCard from './MemoizedMusicCard';
+import { useMusic } from '@/hooks/useMusic';
 import { generateUrl } from '@/utils/helpers';
 import { setCurrentList, setCurrentSongIndex, setCurrentTrack } from '@/Redux/features/musicPlayer/musicPlayerSlice';
-import { useMusic } from '@/hooks/useMusic';
 
 const MusicPlayCard = (props: IMusicPlayCardProps) => {
   const { data, name, message, handleLikeToggle } = props;
