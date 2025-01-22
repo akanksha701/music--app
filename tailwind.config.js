e7593ac85ab5024204bcd757679a9b53a24bcde4
akanksha701 <akanksha@upforce.tech>
2025-01-22 11:27:30 +0530

    compilation config changes

---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+// tailwind.config.js
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { nextui } = require('@nextui-org/react');
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Add your app files
+    './components/**/*.{js,ts,jsx,tsx,mdx}', // Add your component files
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: ['class', 'class'],
+  theme: {
+  	extend: {
+  		colors: {
+        'vivid-violet': '#9333ea',
+        'electric-purple': '#5a17dd',
+        'vivid-orange': '#f98f03',
+        'gunmetal': '#0f172a',
+  			primary: {
+  				purple: '#9333ea'
+  			},
+  			secondary: {
+  				'100': '#9353D3',
+  				'200': '#17C964',
+  				'300': '#FFFFFF'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
+  },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [nextui(), require('tailwind-scrollbar')],
+};
